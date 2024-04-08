e31cf8578b3684c72a6f4c90c2c250e219d25b82
Stefan Rickauer <stefanrick@gmx.de>
2024-04-08 19:48:18 +0200

    Initial commit

---
+++ src/com/rickauer/houdini/Main.java
@@ -0,0 +1,24 @@
+package com.rickauer.houdini;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+	
+	private static String PROGRAM = "Houdini";
+	private static String VERSION = "0.01";
+	private static String PROGRAM_AND_VERSION = PROGRAM + VERSION;
+	
+	private static Logger mainLogger = LogManager.getLogger(Main.class.getName());
+	
+	public static void main(String[] args) {
+		mainLogger.info(PROGRAM_AND_VERSION + " starting ...");
+		try {
+			System.out.println(PROGRAM + " " + VERSION);
+		} catch (Exception exception) {
+			mainLogger.error(exception.getMessage());
+			exception.printStackTrace();
+		}
+	}
+	
+}
