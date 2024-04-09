28318a3acf229435b2dc1317723683df11e642d5
Stefan Rickauer <stefanrick@gmx.de>
2024-04-09 21:03:38 +0200

    Minor changes, mostly tried out processing library which is not suitable for my needs.

--- src/com/rickauer/houdini/Main.java
+++ src/com/rickauer/houdini/Main.java
@@ -6,15 +6,17 @@ import org.apache.logging.log4j.Logger;
 public class Main {
 	
 	private static String PROGRAM = "Houdini";
-	private static String VERSION = "0.01";
-	private static String PROGRAM_AND_VERSION = PROGRAM + VERSION;
+	private static String VERSION = "0.02";
+	private static String PROGRAM_AND_VERSION = PROGRAM + " " + VERSION;
 	
 	private static Logger mainLogger = LogManager.getLogger(Main.class.getName());
 	
 	public static void main(String[] args) {
+
 		mainLogger.info(PROGRAM_AND_VERSION + " starting ...");
 		try {
-			System.out.println(PROGRAM + " " + VERSION);
+			System.out.println(PROGRAM_AND_VERSION);
+			
 		} catch (Exception exception) {
 			mainLogger.error(exception.getMessage());
 			exception.printStackTrace();
