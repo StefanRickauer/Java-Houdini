15b8927c20b371e4bdf46db44cf6c4439aae3c92
Stefan Rickauer <stefanrick@gmx.de>
2024-08-15 19:25:23 +0200

    You know as is is with these projects: You start one... and all of a sudden, you come up with a better idea. But here we go. At least a little progress.

--- src/com/rickauer/houdini/Main.java
+++ src/com/rickauer/houdini/Main.java
@@ -4,7 +4,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -12,7 +21,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	
 	private static String PROGRAM = "Houdini";
-	private static String VERSION = "0.02";
+	private static String VERSION = "0.0201";
 	private static String PROGRAM_AND_VERSION = PROGRAM + " " + VERSION;
 	
 	private static Logger mainLogger = LogManager.getLogger(Main.class.getName());
@@ -33,14 +42,55 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage stage) {
-		Text msg = new Text("Test Screen.");
-		VBox root = new VBox();
-		root.getChildren().add(msg);
+		// Top node
+		Node top = null;
+		
+		// Left node
+		Node left = null;
+		
+		// Center node for content
+		VBox center = createCenter();
+		
+		// Right node
+		Button okBtn = new Button("OK");
+		Button cancelBtn = new Button("Cancel");
+		
+		okBtn.setMaxWidth(Double.MAX_VALUE);
+		VBox right = new VBox(okBtn, cancelBtn);
+		right.setStyle("-fx-padding: 15;");
+		
+		// Bottom node
+		Label statusLbl = new Label("Status: In development...");
+		HBox bottom = new HBox(statusLbl);
+		BorderPane.setMargin(bottom, new Insets(15, 0, 0, 0));
+		bottom.setStyle("-fx-background-color: lightgrey;" +
+						"-fx-font-size: 8pt;" +
+						"-fx-padding: 15 0 0 0");
+		
+		BorderPane root = new BorderPane(center, top, right, bottom, left);
+		root.setStyle("-fx-background-color: yellow;");
 		
 		Scene scene = new Scene(root, 300, 50);
 		stage.setScene(scene);
 		stage.setMaximized(true);
 		stage.setTitle(PROGRAM_AND_VERSION);
 		stage.show();
+	}
+	
+	private VBox createCenter() {
+		Label nameLbl = new Label("Placehodler");
+		TextField nameFld = new TextField();
+		HBox.setHgrow(nameFld, Priority.ALWAYS);
+		HBox nameFields = new HBox(nameLbl, nameFld);
+		
+		Label inputLbl = new Label("Enter your message here:");
+		TextArea inputText = new TextArea();
+		inputText.setPrefColumnCount(20);
+		inputText.setPrefRowCount(5);
+		VBox.setVgrow(inputText, Priority.ALWAYS);
+		
+		VBox center = new VBox(nameFields, inputLbl, inputText);
+		
+		return center;
 	}
 }
